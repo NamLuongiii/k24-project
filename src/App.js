9fa001d782a760d0ef0ce8541b5af184814c07c8
Âu Phươc Vũ <auphuocvu@MacBook-Air-cua-Pham.local>
2022-05-30 17:27:24 +0700

    update interface create order

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import CreateOrder from "./pages/CreateOrder";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<h1>Home page</h1>}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/createOrder" element={<CreateOrder />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
